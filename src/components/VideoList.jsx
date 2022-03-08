5b06a1f4e895da508c4a019d232e76fb24b4e92d
Daniel Esquivel-Reynoso <daniel.esquivelreynoso@gmail.com>
2022-03-07 17:30:00 -0800

    Completed until API integration

--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -1,12 +1,17 @@
-var VideoList = () => (
+import VideoListEntry from './VideoListEntry.js'
+
+var VideoList = (props) => (
   <div className="video-list">
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
+    {props.videos.map(({snippet}) =>
+      <VideoListEntry video={{snippet}} />
+    )}
   </div>
 );
+
+
+// this.data.map(
+//   ({ username, text, roomname }) => ({ username, text, roomname, })
+// );
 
 // PropTypes tell other developers what `props` a component expects
 // Warnings will be shown in the console when the defined rules are violated
